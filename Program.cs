32c59ea9e14da245c13e74e1c463fd0191c65722
emad2262 <105048885+emad2262@users.noreply.github.com>
2025-01-05 17:40:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int small = 25;
+            int large = 35;
+            double tax = 6.6;
+            int smallCarbet = Convert.ToInt32( Console.ReadLine() );
+            Console.WriteLine($"number of small carpets: {smallCarbet}");
+           
+            int largeCarbet = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"number of large carpets: {largeCarbet}");
+
+            Console.WriteLine("price per small : 25$");
+            Console.WriteLine("price per large is : 35$");
+            Console.WriteLine($"cost : {smallCarbet * small + largeCarbet * large}$");
+            Console.WriteLine($"tax is: {tax}$");
+            Console.WriteLine($"total estmate is: {smallCarbet * small + largeCarbet * large + tax}$");
+            Console.WriteLine("this estimate is valid for 30 days ");
+
+        }
+    }
+}
